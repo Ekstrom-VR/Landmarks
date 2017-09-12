f3d194ba41461d6ca04eed055462043f84ee4147
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-09-12 14:16:48 -0700

    Adds regex helper methods

---
+++ Assets/Landmarks/scripts/General/RegexHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+public static class RegexHelpers
+{
+    const string FOLDER_FILE_PATTERN = "^(.+)[/|'\']([^/|'\']+)$";
+
+    public static string GetFolderFromFilepath(string filepath)
+    {
+        var matches = Regex.Match(filepath, FOLDER_FILE_PATTERN);
+        //Group 0 is the full match
+        return matches.Groups[1].Value;
+    }
+
+    public static string GetFileFromFilepath(string filepath)
+    {
+        var matches = Regex.Match(filepath, FOLDER_FILE_PATTERN);
+        //Group 0 is hte full match
+        return matches.Groups[2].Value;
+    }
+}
